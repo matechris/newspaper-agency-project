c6714fc6fc92a5e441877e957a26d2976430b7aa
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-11-10 16:55:18 +0200

    Add urls for newspaper, redactor & topic list view

--- bureau/urls.py
+++ bureau/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 
-from bureau.views import index
+from bureau.views import (
+    index,
+    NewspaperListView,
+    TopicListView,
+    RedactorListView,
+)
 
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "newspapers/",
+        NewspaperListView.as_view(),
+        name="newspaper-list"
+    ),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list"
+    ),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactor-list"
+    ),
 ]
 
 app_name = "bureau"
