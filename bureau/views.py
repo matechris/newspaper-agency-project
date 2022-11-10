241625d0112068dc980dd8d9644319a00c7c4923
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-11-10 16:32:12 +0200

    Create a view fot the home page

--- bureau/views.py
+++ bureau/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from bureau.models import Newspaper, Redactor, Topic
+
+
+def index(request) -> HttpResponse:
+    num_redactors = Redactor.objects.count()
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers
+    }
+
+    return render(request, "bureau/index.html", context=context)
