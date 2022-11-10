ad1792e7da276838e386b1b38c960a9a24311ec1
Khrystyna Skrypko <christina.skrypko@gmail.com>
2022-11-10 20:34:36 +0200

    Create RedactorCreationForm, RedactorExperienceUpdateForm

---
+++ bureau/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from bureau.models import Redactor
+
+
+class RedactorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name", "last_name", "years_of_experience",
+        )
+
+
+class RedactorExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Redactor
+        fields = ("years_of_experience",)
+
+    def clean_years_of_experience(self):
+        years = self.cleaned_data["years_of_experience"]
+
+        if years < 0:
+            raise ValidationError(
+                "Ensure that years value is >= 0"
+            )
+
+        return years
